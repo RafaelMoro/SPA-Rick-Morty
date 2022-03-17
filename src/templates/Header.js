7abf753d77feeb450130bb759f778a1955810c17
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-03-16 23:11:29 -0600

    [FIX] - The styles of the header has been fixed. Some colors has been changed

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,13 +2,9 @@ import logo from '@images/logo.png'
 
 const Header = () => {
     const view = `
-    <header class="header">
-        <h1>
-            <a href="#">
-                <img class="logo" src=${logo} alt="Rick and Morty logo">
-            </a>
-        </h1>
-    </header>
+    <a class="header__anchor" href="#">
+        <img class="logo" src=${logo} alt="Rick and Morty logo">
+    </a>
     `
     return view
 }
