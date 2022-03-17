7aa34feadef4229c90d4c4529563c0d86bb1a8f6
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-03-17 00:30:16 -0600

    The styles of the cards were modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,15 @@ import { getData } from "../utils/getData"
 
 const Home = async() => {
     const characters = await getData()
+    console.log(characters)
     const view = `
     <div class="characters">
         ${characters.results.map(character => `
         <article class="character">
-            <a href="#/${character.id}/">
+            <a class="character__anchor" href="#/${character.id}/">
                 <img class="character__image" src="${character.image}" alt="${character.name}">
                 <h2 class="character__title">${character.name}</h2>
+                <span class="character__status">${character.status}</span>
             </a>
         </article>
         `).join('')}
