80e3b2d7be15903b393b8fe8137360150f048fd5
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-03-18 15:42:34 -0600

    [ADD] - Animation.css has been added and the character card has been animated

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,22 +1,23 @@
 import { getHash } from "../utils/getHash"
 import { getData } from "../utils/getData"
+import 'animate.css'
 
 const Character = async() => {
     const id = getHash()
     const character = await getData(id)
     const view = `
     <article class="character-card">
-        <div class="character-card__image">
+        <div class="animate__animated animate__fadeIn character-card__image">
             <img src="${character.image}" alt="${character.name}">
-            <h2>${character.name}</h2>
+            <h2 class="animate__animated animate__slideInDown">${character.name}</h2>
         </div>
-        <div class="character-card__description">
-            <p><span>Episodes:</span>  ${character.episode.length} </p>
-            <p><span>Status:</span>  ${character.status} </p>
-            <p><span>Species:</span>  ${character.species} </p>
-            <p><span>Gender:</span>  ${character.gender} </p>
-            <p><span>Origin:</span>  ${character.origin.name} </p>
-            <p><span>Last location:</span>  ${character.location.name} </p>
+        <div class="animate__animated animate__fadeIn character-card__description">
+            <p class="animate__animated animate__slideInDown"><span >Episodes:</span>  ${character.episode.length} </p>
+            <p class="animate__animated animate__slideInDown"><span>Status:</span>  ${character.status} </p>
+            <p class="animate__animated animate__slideInDown"><span>Species:</span>  ${character.species} </p>
+            <p class="animate__animated animate__slideInDown"><span>Gender:</span>  ${character.gender} </p>
+            <p class="animate__animated animate__slideInDown"><span>Origin:</span>  ${character.origin.name} </p>
+            <p class="animate__animated animate__slideInDown"><span>Last location:</span>  ${character.location.name} </p>
         </div>
     </article>
     `
